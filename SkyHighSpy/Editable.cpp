201e10bc0632e74a61f21de9ecd813e2396eb0a5
sumo-academy-admin <academy@sumo-digital.com>
2021-03-28 22:01:46 +0100

    Additional comments. Used double literal.

--- SkyHighSpy/Editable.cpp
+++ SkyHighSpy/Editable.cpp
@@ -22,25 +22,25 @@ int DefaultNumMeteors()
 // Controls the initial movement speed of the spider when flying
 float DefaultPlayerSpeed()
 {
-	return 10.0f;
+	return 10.0;
 }
 
 // Controls the movement speed of asteroids
 float DefaultAsteroidSpeed()
 {
-	return 4.0f;
+	return 4.0;
 }
 
 // Controls the movement speed of fiery meteors
 float DefaultMeteorSpeed()
 {
-	return 10.0f;
+	return 10.0;
 }
 
 // Controls the movement of the rocket ship in the tutorial mode
 void UpdatePlayerMovement( float& x, float& y, float& angle, float& speed )
 {
-	
+
 }
 
 // Controls the movement of lasers in the tutorial mode
@@ -52,5 +52,13 @@ void UpdateLaserMovement( float& x, float& y, float& angle, float& speed )
 // Tests to see if a laser has collided with an asteroid
 bool IsLaserCollidingWithAsteroid( float laserX, float laserY, float asteroidX, float asteroidY )
 {
+	float xDiff = asteroidX - laserX;
+	float yDiff = asteroidY - laserY;
+
+	// Can you declare a new float variable called dist and us it to calculate the distance?
+
+	// Then you need to return true if dist is small enough to be considered to be colliding.
+
 	return false;
+	// Don't put any code after the return - it won't do anything!
 }
