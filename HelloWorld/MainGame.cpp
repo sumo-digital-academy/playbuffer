00d8271625f8d2ff59327344a89c778be195f31e
har00n-haider <72197495+har00n-haider@users.noreply.github.com>
2022-08-17 09:57:27 +0300

    adding the robot

--- HelloWorld/MainGame.cpp
+++ HelloWorld/MainGame.cpp
@@ -8,34 +8,37 @@ int DISPLAY_SCALE = 1;
 
 struct GameState
 {
-	float timer = 0;
-	int spriteId = 0;
+	int score = 0;
 };
 
 GameState gameState;
 
+enum GameObjectType
+{
+	TYPE_NULL = -1,
+	TYPE_AGENT8,
+};
+
+void HandlePlayerControls();
 
 // The entry point for a PlayBuffer program
 void MainGameEntry( PLAY_IGNORE_COMMAND_LINE )
 {
 	Play::CreateManager( DISPLAY_WIDTH, DISPLAY_HEIGHT, DISPLAY_SCALE );
+	Play::CentreAllSpriteOrigins();
+	Play::LoadBackground("Data\\Backgrounds\\background.png");
+	Play::StartAudioLoop("music");
+	Play::CreateGameObject(TYPE_AGENT8, { 115,0 }, 50, "agent8");
+
 }
 
 // Called by PlayBuffer every frame (60 times a second!)
 bool MainGameUpdate( float elapsedTime )
 {
-	gameState.timer += elapsedTime;
-	Play::ClearDrawingBuffer( Play::cOrange );
-	Play::DrawDebugText({ DISPLAY_WIDTH / 2, DISPLAY_HEIGHT / 2 },
-						Play::GetSpriteName(gameState.spriteId),
-						Play::cWhite);
-	Play::DrawSprite(gameState.spriteId, Play::GetMousePos(), gameState.timer);
-	
-	if (Play::KeyPressed(VK_SPACE))
-		gameState.spriteId++;
-	
+	Play::DrawBackground();
+	HandlePlayerControls();
 	Play::PresentDrawingBuffer();
-	return Play::KeyDown( VK_ESCAPE );
+	return Play::KeyDown(VK_ESCAPE);
 }
 
 // Gets called once when the player quits the game 
@@ -45,3 +48,30 @@ int MainGameExit( void )
 	return PLAY_OK;
 }
 
+void HandlePlayerControls()
+{
+	GameObject& obj_agent8 = Play::GetGameObjectByType(TYPE_AGENT8);
+	if (play::KeyDown(VK_UP))
+	{
+		obj_agent8.velocity = { 0,-4 };
+		Play::SetSprite(obj_agent8, "agent8_climb", 0.25f);
+	}
+	else if (Play::KeyDown(VK_DOWN))
+	{
+		obj_agent8.acceleration = { 0,1 };
+		Play::SetSprite(obj_agent8, "agent8_fall", 0);
+	}
+	else
+	{
+		Play::SetSprite(obj_agent8, "agent8_hang", 0.02f);
+		obj_agent8.velocity *= 0.5f;
+		obj_agent8.acceleration = { 0,0 };
+	}
+	Play::UpdateGameObject(obj_agent8);
+
+	if (Play::IsLeavingDisplayArea(obj_agent8))
+		obj_agent8.pos = obj_agent8.oldPos;
+
+	Play::DrawLine({ obj_agent8.pos.x,0 }, obj_agent8.pos, Play::cWhite);
+	Play::DrawObjectRotated(obj_agent8);
+}
